d3f63288d71b7c50f53ea2dc9ac0f776ccd036ec
Greg Chapple <gregchapple1@gmail.com>
2015-03-02 10:12:48 +0000

    Use c_int instead of c_uint to be more inline with termbox

--- lib.rs
+++ lib.rs
@@ -1,7 +1,7 @@
 #![feature(libc)]
 extern crate libc;
 
-use libc::{c_int, c_uint};
+use libc::c_int;
 
 #[repr(C)]
 #[derive(Copy)]
@@ -20,18 +20,18 @@ extern "C" {
     pub fn tb_init() -> c_int;
     pub fn tb_shutdown();
 
-    pub fn tb_width() -> c_uint;
-    pub fn tb_height() -> c_uint;
+    pub fn tb_width() -> c_int;
+    pub fn tb_height() -> c_int;
 
     pub fn tb_clear();
     pub fn tb_present();
 
     pub fn tb_set_cursor(cx: c_int, cy: c_int);
-    pub fn tb_change_cell(x: c_uint, y: c_uint, ch: u32, fg: u16, bg: u16);
+    pub fn tb_change_cell(x: c_int, y: c_int, ch: u32, fg: u16, bg: u16);
 
     pub fn tb_select_input_mode(mode: c_int) -> c_int;
     pub fn tb_set_clear_attributes(fg: u16, bg: u16);
 
-    pub fn tb_peek_event(ev: *const ::RawEvent, timeout: c_uint) -> c_int;
+    pub fn tb_peek_event(ev: *const ::RawEvent, timeout: c_int) -> c_int;
     pub fn tb_poll_event(ev: *const ::RawEvent) -> c_int;
 }
